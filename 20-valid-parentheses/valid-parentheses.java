c3344b356682886e06035ffe4475039974d4b32b
vanpcoding-oss <vanp.coding@gmail.com>
2026-04-22 21:51:36 +0530

    Time: 4 ms (40.98%) | Memory: 43.1 MB (86.22%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character,Character> map=new HashMap<>();
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toCharArray())   // ()->['(',')'];
+        {
+            if(map.containsKey(ch))
+            {
+                if(!stack.isEmpty() && stack.peek()==map.get(ch))
+                {
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
